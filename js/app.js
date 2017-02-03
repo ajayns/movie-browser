149bf761b2ae0dde7695b28dd8698dbc126ce266
Ajay NS <ajay.ns@outlook.com>
2017-02-03 16:42:24 +0530

    Minor fixes, merge plunker code

--- js/app.js
+++ js/app.js
@@ -1,24 +1,33 @@
-var app = angular.module('movieBrowser', []);
+var app = angular.module('movBro', []).controller('mainCtrl', function($scope, $http) {
 
-app.controller('mainCtrl', function($scope, $http) {
-
-  $scope.$watch('searchTerm', function() {
+  $scope.$watch('searchTerm', function(){
     fetch();
-  })
+  });
+
+  $scope.searchTerm = "The Dark Knight";
 
   function fetch(){
     $http.get("https://www.omdbapi.com/?t=" + $scope.searchTerm + "&tomatoes=true&plot=full")
-      .then(function(response){ $scope.details = response });
+      .then(function(response){ $scope.details = response.data });
 
-    $http.get("https://www.omdbapi.com/?s="$scope.searchTerm)
-      .then(function(response){ $scope.related = response });
+    $http.get("https://www.omdbapi.com/?s=" + $scope.searchTerm)
+      .then(function(response){ $scope.related = response.data });
   }
 
   $scope.select = function() {
     this.setSelectionRange(0, this.value.length);
-  }
+  };
 
   $scope.update = function(movie) {
     $scope.searchTerm = movie.Title;
-  }
+  };
+});
+
+app.directive('ngBg', function() {
+  return function(scope, element, attrs) {
+    var url = attrs.ngBg;
+    element.css({
+      'background-image': 'url(' + url + ')'
+    });
+  };
 });
